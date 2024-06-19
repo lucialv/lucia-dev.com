574db5f329ee0bb709153f7330f29e8fb681cb5b
lucialv <lucia.alvrzt@gmail.com>
2024-06-19 15:12:32 +0200

    Improved SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://lucia-dev.com",
+  integrations: [
+    tailwind(),
+    sitemap({
+      i18n: {
+        defaultLocale: "es",
+        locales: {
+          es: "es-ES", // default locale
+          en: "en-US",
+        },
+      },
+    }),
+  ],
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
