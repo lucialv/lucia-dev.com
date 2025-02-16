d576d0ef69f61aa3e940409379de02d7a29f4e52
lucialv <lucia.alvrzt@gmail.com>
2025-02-16 16:32:20 +0100

    upgraded astro to version 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
